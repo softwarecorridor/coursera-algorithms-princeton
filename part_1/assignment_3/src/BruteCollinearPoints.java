23816bae1ae9b0b4d8e196af4af7919fe70e202d
softwarecorridor <softwarecorridor@users.noreply.github.com>
2017-05-30 22:55:11 -0400

    move to storing point information in linked list

--- part_1/assignment_3/src/BruteCollinearPoints.java
+++ part_1/assignment_3/src/BruteCollinearPoints.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class BruteCollinearPoints {
 
-	private ArrayList<LineSegment> segments = new ArrayList<>();
+	private ArrayList<Node> nodes = new ArrayList<>();
 	
 	
 	/**
@@ -31,17 +31,23 @@ public class BruteCollinearPoints {
 		{
 			
 			Point pPoint = points[p];
-			System.out.println(pPoint.toString());
+			
+			Node currentLineSegment = new Node(p);
+			boolean isNewLine = true;
+			
 			for (int q = p+ 1; q<points.length-2;q++)
 			{
 				
 				Point qPoint = points[q];
 				double p_q = pPoint.slopeTo(qPoint);
 				
+				
 				for (int r = q + 1; r<points.length-1;r++)
 				{
 					Point rPoint = points[r];
-					double p_r = pPoint.slopeTo(rPoint);	
+					double p_r = pPoint.slopeTo(rPoint);
+					
+					
 					if(p_r == p_q)
 					{
 						for (int s =r + 1; s<points.length;s++)
@@ -52,13 +58,51 @@ public class BruteCollinearPoints {
 							
 							if (p_r == p_s)
 							{
-								segments.add(new LineSegment(pPoint, sPoint));
+								if(isNewLine)
+								{
+									nodes.add(currentLineSegment);
+									
+									currentLineSegment.next = new Node(q);
+									currentLineSegment = currentLineSegment.next;
+									currentLineSegment.next = new Node(r);
+									currentLineSegment = currentLineSegment.next;
+									
+									
+									isNewLine = false;
+									
+								}else
+								{
+									if(!Node.containsNode(nodes.get(p), s))
+									{
+										currentLineSegment.next = new Node(s);
+										currentLineSegment = currentLineSegment.next;
+									}
+								}
+								
+								
+								
+								
 							}
 						}
 					}
 				}
 			}
 		}
+		
+		
+		
+		if(nodes.size()>1)
+		{
+			for (int i = 1; i<nodes.size(); i++)
+			{
+				if(Node.collinear(nodes.get(i-1), nodes.get(i)))
+				{
+					nodes.remove(nodes.get(i));
+				}
+			}
+		}
+		
+		debug();
 	}
 	
 
@@ -69,7 +113,7 @@ public class BruteCollinearPoints {
 	 * @return
 	 */
 	public int numberOfSegments() {
-		return segments.size();
+		return nodes.size();
 	}
 
 	/**
@@ -79,9 +123,94 @@ public class BruteCollinearPoints {
 	 * @return
 	 */
 	public LineSegment[] segments() {
-		return segments.toArray(new LineSegment[0]);
+		return  null;
 	};
 	
+	private static class Node
+	{
+		public int position;
+		public Node next;
+		
+		
+		public Node(int pos)
+		{
+			position = pos;
+			next = null;
+		}
+		
+		
+		public static Node searchNode(Node n, int pos)
+		{
+			Node current = n;
+			while(current.next != null)
+			{
+				if(current.position == pos)
+				{
+					return current;
+				}
+				current = current.next;
+			}
+			return null;
+		}
+		
+		public static boolean containsNode(Node n, int pos)
+		{
+			Node current = n;
+			while(current.next!= null)
+			{
+				if(current.position == pos)
+				{
+					return true;
+				}
+				current = current.next;
+			}
+			return false;
+		}
+		
+		public static int size(Node n)
+		{
+			int count = 0;
+			Node current = n;
+			while(current.next!= null)
+			{
+				count++;
+				current = current.next;
+			}
+			return count;
+		}
+		
+		public static boolean collinear(Node outer, Node inner)
+		{
+			Node coNode = Node.searchNode(outer, inner.position);
+			
+			if(coNode!=null)
+			{
+				if(coNode.next != null && inner.next != null)
+				{
+					return coNode.next.position == inner.next.position;
+				}
+			}
+			
+			return false;
+		}
+			
+	}
+	
+	private void debug()
+	{
+		for(Node n: nodes)
+		{
+			Node current = n;
+			StringBuilder sb = new StringBuilder();
+			while(current.next!= null)
+			{
+				sb.append(current.position);
+				sb.append("=>");
+				current = current.next;
+			}
+			System.out.println(sb.toString());
+		}
+	}
 	
 	
 
