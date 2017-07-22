e753536413da07c2fc8d281a32c0293963dea1fa
softwarecorridor <softwarecorridor@users.noreply.github.com>
2017-07-22 18:19:32 -0400

    can remove one loop from detect duplicate since the inner loop will check the last section

--- part_1/assignment_3/src/BruteCollinearPoints.java
+++ part_1/assignment_3/src/BruteCollinearPoints.java
@@ -4,55 +4,45 @@ import java.util.Arrays;
 public class BruteCollinearPoints {
 
 	private ArrayList<LineSegment> segments = new ArrayList<>();
-	
-	
+
 	/**
-	 * finds all line segments containing 4 points.
-	 * 4 points at a time and checks whether they all lie on the same line segment, 
-	 * returning all such line segments. 
-	 * To check whether the 4 points p, q, r, and s are collinear, 
-	 * check whether the three slopes between p and q, between p and r, 
-	 * and between p and s are all equal. 
+	 * finds all line segments containing 4 points. 4 points at a time and
+	 * checks whether they all lie on the same line segment, returning all such
+	 * line segments. To check whether the 4 points p, q, r, and s are
+	 * collinear, check whether the three slopes between p and q, between p and
+	 * r, and between p and s are all equal.
 	 * 
 	 * @param points
 	 */
 	public BruteCollinearPoints(Point[] points) {
 
-		if(points == null)
-		{
+		if (points == null) {
 			throw new NullPointerException();
 		}
-		
+
 		detectDuplicatedPoints(points);
-		
+
 		Arrays.sort(points);
-		
-		
-		for (int p = 0; p<points.length-3;p++)
-		{
-			
+
+		for (int p = 0; p < points.length - 3; p++) {
+
 			Point pPoint = points[p];
 			System.out.println(pPoint.toString());
-			for (int q = p+ 1; q<points.length-2;q++)
-			{
-				
+			for (int q = p + 1; q < points.length - 2; q++) {
+
 				Point qPoint = points[q];
 				double p_q = pPoint.slopeTo(qPoint);
-				
-				for (int r = q + 1; r<points.length-1;r++)
-				{
+
+				for (int r = q + 1; r < points.length - 1; r++) {
 					Point rPoint = points[r];
-					double p_r = pPoint.slopeTo(rPoint);	
-					if(p_r == p_q)
-					{
-						for (int s =r + 1; s<points.length;s++)
-						{
-							
+					double p_r = pPoint.slopeTo(rPoint);
+					if (p_r == p_q) {
+						for (int s = r + 1; s < points.length; s++) {
+
 							Point sPoint = points[s];
 							double p_s = pPoint.slopeTo(sPoint);
-							
-							if (p_r == p_s)
-							{
+
+							if (p_r == p_s) {
 								segments.add(new LineSegment(pPoint, sPoint));
 							}
 						}
@@ -61,19 +51,14 @@ public class BruteCollinearPoints {
 			}
 		}
 	}
-	
-	
-	private void detectDuplicatedPoints(Point[] points)
-	{
-		for (int i = 0; i< points.length; i++)
-		{
-			for (int j = i+1; j < points.length; j++)
-			{
+
+	private void detectDuplicatedPoints(Point[] points) {
+		for (int i = 0; i < points.length-1; i++) {
+			for (int j = i + 1; j < points.length; j++) {
 				Point p = points[i];
 				Point q = points[j];
-				
-				if(p.compareTo(q) == 0)
-				{
+
+				if (p.compareTo(q) == 0) {
 					throw new IllegalArgumentException();
 				}
 			}
@@ -99,8 +84,5 @@ public class BruteCollinearPoints {
 	public LineSegment[] segments() {
 		return segments.toArray(new LineSegment[0]);
 	};
-	
-	
-	
 
 }
