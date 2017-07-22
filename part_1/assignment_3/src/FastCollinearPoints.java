26f6c91eb5ae7044d0a48b2c3d3f2d08e1cab659
softwarecorridor <softwarecorridor@users.noreply.github.com>
2017-07-22 17:05:08 -0400

    implement FastCollinearPoints

--- part_1/assignment_3/src/FastCollinearPoints.java
+++ part_1/assignment_3/src/FastCollinearPoints.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FastCollinearPoints {
+
+	private ArrayList<LineSegment> segments;
 
 	/**
 	 * finds all line segments containing 4 or more points
@@ -7,6 +11,70 @@ public class FastCollinearPoints {
 	 * @param points
 	 */
 	public FastCollinearPoints(Point[] points) {
+		
+		if(points == null)
+		{
+			throw new NullPointerException();
+		}
+		
+		detectDuplicatedPoints(points);
+		
+
+		segments = new ArrayList<>();
+
+		for (int p = 0; p < points.length - 3; p++) {
+			// Think of p as the origin.
+			Point origin = points[p];
+
+			// sorted by
+			Point[] copy = points;
+			// Sort the points according to the slopes they makes with p.
+			Arrays.sort(copy, origin.slopeOrder());
+
+			// Check if any 3 (or more) adjacent points in the sorted order have
+			// equal slopes with respect to p. If so, these points, together
+			// with p, are
+			// collinear.
+			for (int j = 1; j < copy.length - 3; j++) {
+				Point pointJ = copy[j];
+				Point pointK = copy[j + 1];
+				Point pointL = copy[j + 2];
+
+				double slopeJ = origin.slopeTo(pointJ);
+				double slopeK = origin.slopeTo(pointK);
+				double slopeL = origin.slopeTo(pointL);
+
+				if (slopeJ == slopeK && slopeK == slopeL) {
+					addSegment(origin, pointL);
+				}
+
+			}
+
+		}
+
+	}
+	
+	private void detectDuplicatedPoints(Point[] points)
+	{
+		for (int i = 0; i< points.length; i++)
+		{
+			for (int j = i+1; j < points.length; j++)
+			{
+				Point p = points[i];
+				Point q = points[j];
+				
+				if(p.compareTo(q) == 0)
+				{
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+	}
+
+
+	private void addSegment(Point start, Point end) {
+
+		segments.add(new LineSegment(start, end));
 	}
 
 	/**
@@ -15,8 +83,8 @@ public class FastCollinearPoints {
 	 * @return
 	 */
 	public int numberOfSegments() {
-		return -1;
-	} 
+		return segments.size();
+	}
 
 	/**
 	 * the line segments
@@ -24,7 +92,7 @@ public class FastCollinearPoints {
 	 * @return
 	 */
 	public LineSegment[] segments() {
-		return null;
-	}
+		return segments.toArray(new LineSegment[0]);
+	};
 
 }
